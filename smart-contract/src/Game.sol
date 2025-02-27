e40b4834e290f34eaf93baf63142536895ddf1bf
Kudaru Deborah Nshuk <deborahkudaru@gmail.com>
2025-02-27 22:56:16 +0100

    create player in sol

--- smart-contract/src/Game.sol
+++ smart-contract/src/Game.sol
@@ -2,5 +2,21 @@
 pragma solidity ^0.8.13;
 
 contract Game {
-    
+
+
+
+   
+    struct Player {
+        address playerAddress;
+        string username;
+        uint256 totalPoints;
+        uint256 correctPredictions;
+        uint256 totalPredictions;
+    }
+
+   mapping(address => Player) public players;
+
+   function createPlayer(string memory _username)external{
+      
+   }
 }
