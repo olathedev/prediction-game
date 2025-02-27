2e4a503a0b089500becb3f65c61a71fd1373fa5e
olathedev <olapademicheal3@gmail.com>
2025-02-27 22:25:05 +0100

    add constructor and modifier in Game.sol

--- smart-contract/src/Game.sol
+++ smart-contract/src/Game.sol
@@ -2,5 +2,14 @@
 pragma solidity ^0.8.13;
 
 contract Game {
-    
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Game: only owner can call this function");
+        _;  
+    }
 }
