09fb6e237d7b6ecbb14ee61510bd1bd28aceb86b
Kudaru Deborah Nshuk <deborahkudaru@gmail.com>
2025-03-01 13:55:46 +0100

    spacing

--- frontend/src/components/LeaderBoard.tsx
+++ frontend/src/components/LeaderBoard.tsx
@@ -6,8 +6,8 @@ const LeaderBoard = () => {
    ];
  
    return (
-     <div className="w-full bg-[#0f1420] flex justify-center p-6">
-       <div className="w-full max-w-3xl bg-[#080c14] text-white rounded-2xl shadow-lg p-6">
+     <div className="w-full bg-[#0f1420] flex justify-center">
+       <div className="w-full bg-[#080c14] text-white rounded-2xl shadow-lg p-6">
          <h1 className="text-center text-3xl font-bold mb-6">🏆 LeaderBoard</h1>
          <table className="w-full text-white border-separate border-spacing-y-3">
            <thead>
--- frontend/src/components/Question.tsx
+++ frontend/src/components/Question.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const Question = ( { question } : Props ) => {
   return (
-    <div className="bg-[#080c14] bg-opacity-80 backdrop-blur-xl p-6 lg:w-3/5 rounded-xl flex flex-col space-y-5 text-white">
+    <div className="bg-[#080c14] bg-opacity-80 backdrop-blur-xl p-6 rounded-xl flex flex-col space-y-5 text-white">
       {/* <div className="bg-gradient-to-r text-sm from-purple-400 via-pink-500 to-red-500 text-transparent bg-clip-text">120d 9h 48m 37s</div> */}
       <h2 className="text-gray-300">
         { question }
