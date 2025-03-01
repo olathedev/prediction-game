18cb3d697b60b93cc5baf2613abb5bf7f70d7b0e
Kudaru Deborah Nshuk <deborahkudaru@gmail.com>
2025-03-01 12:53:22 +0100

    leaderboard

--- frontend/src/components/Question.tsx
+++ frontend/src/components/Question.tsx
@@ -13,35 +13,7 @@ const Question = ( { question } : Props ) => {
       </h2>
 
       <div className="flex text-sm text-gray-400">
-        <div className="flex items-center gap-1 ">
-          <span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-4 text-green-500"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 18 9 11.25l4.306 4.306a11.95 11.95 0 0 1 5.814-5.518l2.74-1.22m0 0-5.94-2.281m5.94 2.28-2.28 5.941"
-              />
-            </svg>
-          </span>
-          {/* <p className="font-medium"></p> */}
-        <p className="inline-flex gap-1 text-xs">
-            Yes (<span className="">2%</span>)
-        </p>
-        -
-        <p className="inline-flex gap-1 text-xs">
-            No (<span className="">50%</span>)
-        </p>
-
-        ROI
-        </div>
-
+     
       </div>
 
       <div className="w-full flex gap-4">
