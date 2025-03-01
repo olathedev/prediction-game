54bc06c332a6a71ff1b80be79d8004ce10ba0067
olathedev <olapademicheal3@gmail.com>
2025-03-01 08:41:46 +0100

    setup react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 // import Body from "./components/Body"
 
+import { Route, Routes } from "react-router-dom"
 import Admin from "./components/Admin"
+import Body from "./components/Body"
 
 function App() {
   return (
     <main className="min-h-screen max-w-3xl mx-auto h-screen p-4">
+
+      <Routes>
+        <Route path="/" element={<Body />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
       {/* <Body /> */}
-      <Admin />
+      {/* <Admin /> */}
     </main>
   )
 }
