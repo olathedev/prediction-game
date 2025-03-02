96149a6ec204bf5ff31d1c18b30246af9f412c20
olathedev <olapademicheal3@gmail.com>
2025-03-02 14:27:14 +0100

    iplement navbar

--- frontendd/src/components/NavBar.tsx
+++ frontendd/src/components/NavBar.tsx
@@ -1,9 +1,18 @@
 const NavBar = () => {
   return (
-    <div className="w-full bg-custom-gradient h-10">
-      
-    </div>
-  )
-}
+    <div className="container flex items-center justify-between md:mx-auto my-6 bg-[#2463FF]/40 py-2 px-6 rounded-full">
+      <div className="rounded-[2.5rem]  py-3 flex items-center gap-2 text-xl">
+        <img src="/images/trophy-svgrepo-com.svg" className="h-5" />
+        <p>LeaderBoard</p>
+      </div>
 
-export default NavBar
+      <div className="flex text-lg">
+        <p>@Username</p>
+
+        
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
