ef6e46ffa4c613a2bf5e30e168cefe577f1c0fa4
Emeka Rex Chukwu <emekarexchukwu@gmail.com>
2025-03-06 01:52:50 +0100

    Revert "feature: modify gameLevels"
    This reverts commit 3c5d35ed0c87288865658fe886e45c90defe1917.

--- hardhat-contract/contracts/Guess.sol
+++ hardhat-contract/contracts/Guess.sol
@@ -7,7 +7,6 @@ import "./lib/Errors.sol";
 contract GuessGame {
     // State Variables
     address public owner;
-    uint8 public constant MAX_NUMBER_OF_LEVELS = 10;
     uint256 public constant QUESTIONS_PER_GAME = 10;
     uint256 public constant POINTS_PER_CORRECT_ANSWER = 10;
     uint256 public constant STREAK_REWARD_POINTS = 50;
@@ -21,7 +20,7 @@ contract GuessGame {
         uint256 totalCorrect;
         uint256 currentStreak;
         uint256 stakedAmount;
-        // bool hasPlayed;
+        bool hasPlayed;
     }
 
     struct GameResult {
@@ -33,7 +32,6 @@ contract GuessGame {
 
     mapping(address => Player) public players;
     address[] public allPlayers;
-    mapping(address => uint8) public gameLevels;
     mapping(uint256 => GameResult[]) public gameResults; // Per-game leaderboard
     mapping(address => uint256[QUESTIONS_PER_GAME]) public userPredictions;
     mapping(address => uint256[QUESTIONS_PER_GAME]) public correctAnswers;
@@ -75,14 +73,12 @@ contract GuessGame {
     }
 
     function submitPredictions(uint256[QUESTIONS_PER_GAME] memory answers) external payable {
-        // require(!players[msg.sender].hasPlayed, "Already played");
-        require(gameLevels[msg.sender] < 10, "Level already completed");
+        require(!players[msg.sender].hasPlayed, "Already played");
         require(msg.value > 0, "Must stake some ETH");
 
         userPredictions[msg.sender] = answers;
         players[msg.sender].stakedAmount = msg.value;
-        // players[msg.sender].hasPlayed = true;
-        gameLevels[msg.sender] += 1;
+        players[msg.sender].hasPlayed = true;
 
         _generateCorrectAnswers();
         emit PredictionsSubmitted(msg.sender, answers);
@@ -90,7 +86,7 @@ contract GuessGame {
 
     function _generateCorrectAnswers() internal {
         address player = msg.sender;
-        // require(players[player].hasPlayed, "Player has not played");
+        require(players[player].hasPlayed, "Player has not played");
 
         uint256[QUESTIONS_PER_GAME] memory generatedAnswers;
         for (uint256 i = 0; i < QUESTIONS_PER_GAME; i++) {
