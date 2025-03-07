bef0459b59426a498747b1c37877d2e7878096cd
olathedev <olapademicheal3@gmail.com>
2025-03-07 01:38:49 +0100

    implement ai aget api

--- frontendd/src/components/InfoScreen.tsx
+++ frontendd/src/components/InfoScreen.tsx
@@ -1,11 +1,35 @@
 import { motion } from "framer-motion";
 import PlayButton from "./Playbutton";
+import { useGame } from "../context/GameContext";
+import { useState } from "react";
 
 interface Props {
   onClose: () => void;
 }
 
 const InfoScreen = ({ onClose }: Props) => {
+  const { getQuestions, setQuestions } = useGame();
+  const [loading, setLoading] = useState(false);
+
+  const onStart = async () => {
+    setLoading(true);
+    try {
+      const res = await fetch(
+        "https://prediction-api-1.onrender.com/api/create-question"
+      );
+      const data = await res.json();
+      setLoading(false);
+      if (res.ok) {
+        setQuestions(data?.questions);
+        onClose();
+      }
+
+      console.log(data);
+    } catch (error) {
+      console.error();
+      setLoading(false);
+    }
+  };
   return (
     <motion.div
       initial={{ opacity: 0, scale: 0.8 }}
@@ -15,14 +39,19 @@ const InfoScreen = ({ onClose }: Props) => {
       className="fixed inset-0 bg-black/50 flex items-center justify-center"
     >
       <div className="bg-custom-gradient p-8 relative flex md:h-[26rem] w-[22rem] md:w-[38rem] flex-col items-center justify-center gap-8 rounded-[3rem] shadow-[inset_0px_-8px_0px_4px_#140E66,inset_0px_6px_0px_8px_#2463FF] mt-12">
-        <h2 className="text-3xl font-bold text-white">Get Ready! Please read!</h2>
+        <h2 className="text-3xl font-bold text-white">
+          Get Ready! Please read!
+        </h2>
         <p className="text-[22px] text-gray-300 text-center">
-          Hey there, Player! in this challanege
-          you'll need to answer 10 predictive questions, and would be required to stake <span className="text-yellow-500">0.2 core tokens</span> you'll get back double of your stake if you get 8 predicitons right.  You've got just 60
-          seconds to showcase your skills and beat the clock or loose your stake, Remember, time
-          waits for no one, so stay sharp and have fun! Good luck!
+          Hey there, Player! in this challanege you'll need to answer 10
+          predictive questions, and would be required to stake{" "}
+          <span className="text-yellow-500">0.2 core tokens</span> you'll get
+          back double of your stake if you get 8 predicitons right. You've got
+          just 60 seconds to showcase your skills and beat the clock or loose
+          your stake, Remember, time waits for no one, so stay sharp and have
+          fun! Good luck!
         </p>
-        <div className="mt-4" onClick={onClose}>
+        <div className="mt-4" onClick={onStart}>
           <PlayButton />
         </div>
       </div>
