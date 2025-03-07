0072216638179829bd1d8dac113b9e46ae6c9d13
olathedev <olapademicheal3@gmail.com>
2025-03-07 20:58:57 +0100

    ui fixes

--- frontendd/src/components/InfoScreen.tsx
+++ frontendd/src/components/InfoScreen.tsx
@@ -2,12 +2,14 @@ import { motion } from "framer-motion";
 import PlayButton from "./Playbutton";
 import { useGame } from "../context/GameContext";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 interface Props {
   onClose: () => void;
 }
 
 const InfoScreen = ({ onClose }: Props) => {
+  const navigate = useNavigate()
   const { setQuestions } = useGame();
   const [loading, setLoading] = useState(false);
   console.log(loading)
@@ -38,7 +40,7 @@ const InfoScreen = ({ onClose }: Props) => {
       transition={{ duration: 0.5, type: "spring", ease: "backInOut" }}
       className="fixed inset-0 bg-black/50 flex items-center justify-center"
     >
-      <div className="bg-custom-gradient p-8 relative flex md:h-[26rem] w-[22rem] md:w-[38rem] flex-col items-center justify-center gap-8 rounded-[3rem] shadow-[inset_0px_-8px_0px_4px_#140E66,inset_0px_6px_0px_8px_#2463FF] mt-12">
+      <div className="bg-custom-gradient p-8 relative flex md:h-[30rem] w-full md:w-[38rem] flex-col items-center justify-center gap-8 rounded-[3rem] shadow-[inset_0px_-8px_0px_4px_#140E66,inset_0px_6px_0px_8px_#2463FF] mt-12">
         <h2 className="text-3xl font-bold text-white">
           Get Ready! Please read!
         </h2>
@@ -51,9 +53,16 @@ const InfoScreen = ({ onClose }: Props) => {
           your stake, Remember, time waits for no one, so stay sharp and have
           fun! Good luck!
         </p>
-        <div className="mt-4" onClick={onStart}>
+        <div className="mt-2" onClick={onStart}>
           <PlayButton />
         </div>
+
+        <button
+          onClick={() => navigate('/')}
+          className=" text-gray-300 cursor-pointer text-xl hover:text-white transition"
+        >
+          Exit game
+        </button>
       </div>
     </motion.div>
   );
